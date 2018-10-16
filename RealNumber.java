052c5b52a4590df85a8378b7b6b6e812421c2d2c
cyee00 <cyee00@stuy.edu>
2018-10-16 10:09:23 -0400

    kd

--- RealNumber.java
+++ RealNumber.java
@@ -1,12 +1,41 @@
-public class RealNumber {
-  private double n;
-  public RealNumber(double i) {
-    n = i;
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
   }
-  public double getRealNumber() {
-    return n;
+
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
+    return this+other;
   }
-  public String toString() {
-    return "RealNumber: "+n;
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return this*other;
+  }
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return this/other;
+  }
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    return this-other;
+  }
+
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+value;
   }
 }
