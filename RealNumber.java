85185a396fc3ccef3914d1595af1717d6ca34221
cyee00 <cyee00@stuy.edu>
2018-10-15 22:33:21 -0400

    real

--- RealNumber.java
+++ RealNumber.java
@@ -1,3 +1,12 @@
 public class RealNumber {
-  
+  private double n;
+  public RealNumber(double i) {
+    n = i;
+  }
+  public double getRealNumber() {
+    return n;
+  }
+  public String toString() {
+    return "RealNumber: "+n;
+  }
 }
