87909c346131ca20b42cdae491ba27061bea7c6a
cyee00 <cyee00@stuy.edu>
2018-10-15 22:45:40 -0400

    Rational

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,3 +1,10 @@
 public class RationalNumber extends RealNumber {
-  
+  private String r;
+  private double num,den;
+  public RationalNumber(double n,double d) {
+    super(n);
+  }
+  public String toString() {
+    return "RationalNumber: "+r;
+  }
 }
